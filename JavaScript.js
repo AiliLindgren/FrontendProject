5fad6684f27c367676653d693d882f3ae1a0d9a8
AiliLindgren <lindgren.aili@gmail.com>
2020-03-31 14:00:37 +0200

    Added function and buttons + -
    (cherry picked from commit bef385b55528e8e9be1518701318c679cfc84b4d)

---
+++ JavaScript.js
@@ -0,0 +1,12 @@
+let amount = 0; // let eller var?
+
+function changeAmount(operator){
+    if (operator === '-' && amount > 0){
+        amount--  
+    }
+    if (operator === '+') {
+        amount++
+    }
+
+    document.getElementById("amount").innerText = amount
+}
